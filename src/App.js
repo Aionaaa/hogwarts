4b1831e7cbf5967700c75e00188c60af62b5b1b6
Iona Grant <ionagrant92@gmail.com>
2019-12-10 15:14:03 +0000

    dropdown works

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
+import CharacterBox from './containers/CharacterBox'
 
 function App() {
   return (
-  
+  <CharacterBox />
   );
 }
 
--- src/containers/CharacterBox.js
+++ src/containers/CharacterBox.js
@@ -1,19 +1,37 @@
 import React, {Component, Fragment} from 'react'
+import CharacterSelector from '../components/CharacterSelector'
+import CharacterDetail from '../components/CharacterDetail'
 
 class CharacterBox extends Component{
   constructor(props){
-    super(props)
+    super(props);
     this.state = {characters: [],
-    currentCharacter: null}
+    currentCharacter: null};
+    this.handleCharacterSelected = this.handleCharacterSelected.bind(this)
   }
-}
+
   componentDidMount(){
     const url = "http://hp-api.herokuapp.com/api/characters";
 
     fetch(url)
     .then(res => res.json())
-    .then(countries => this.setState({countries: countries}))
+    .then(characters => this.setState({characters: characters}))
     .catch(err => console.error);
   }
 
+  handleCharacterSelected(index){
+    const selectedCharacter = this.state.characters[index];
+    this.setState({currentCharacter: selectedCharacter})
+  }
+
+  render(){
+    return(
+      // <h2>CharacterBox</h2>
+      <CharacterSelector
+      characters={this.state.characters}
+      onCharacterSelected={this.handleCharacterSelected}/>
+      // <CharacterDetail character={this.state.currentCharacter}/>
+    );
+  }
+}
 export default CharacterBox;
